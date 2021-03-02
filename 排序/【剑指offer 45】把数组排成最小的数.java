36dfada6722950ffdd767e5e7ba284cc83402520
Zhou Tong <62006510+littlelittlewhite09@users.noreply.github.com>
2021-03-02 12:11:12 +0800

    Create 【剑指offer 45】把数组排成最小的数.java

---
+++ 排序/【剑指offer 45】把数组排成最小的数.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String minNumber(int[] nums) {
+       String[] strs = new String[nums.length];
+        for(int i = 0; i < nums.length; i++) 
+            strs[i] = String.valueOf(nums[i]);
+        Arrays.sort(strs, (x,y) -> (x+y).compareTo(y+x));//这里相当于python中的lambda函数
+        StringBuilder res = new StringBuilder();
+        for(String s : strs)
+            res.append(s);
+        return res.toString();
+
+    }
+}
